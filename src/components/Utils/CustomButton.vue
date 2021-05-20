7c024ea874e1741e54ffa2e656b20248b4ae3db9
Thiago Moura <cbjrcs@hotmail.com>
2021-05-20 03:37:26 -0300

    feature: GS-002 | Criação da Home

---
+++ src/components/Utils/CustomButton.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <button class="button" :value="searchValue" :style="{'background-color':bgColor}" @click="action">{{label}}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    searchValue: {
+      type: String,
+      default: ''
+    },
+    label: {
+      type: String,
+      default: 'Ação'
+    },
+    bgColor: {
+      type: String,
+      default: 'grey'
+    },
+    action: {
+      type: Function,
+      default: () => {}
+    }
+  }
+}
+</script>
+
+<style scoped>
+.button {
+  border-radius: 4px;
+  border: none;
+  height: 50px;
+  width: 40vw;
+  max-width: 200px;
+  padding: 5px 10px;
+  margin: 5px 10px;
+  color: white;
+  text-transform: capitalize;
+}
+</style>
